cb5d4d682d0c7fb9e788c7689556f93db5f9f0bb
obrucheoghene <obrucheoghene1@gmail.com>
2025-08-17 20:16:48 +0100

    add actions

---
+++ src/types/actions.ts
@@ -0,0 +1,24 @@
+export enum SignalingClientActions {
+  Message = 'message',
+  Heartbeat = 'heartbeat',
+  Connected = 'connected',
+  JoinRoom = 'join-room',
+  JoinVisitors = 'join-visitors',
+  JoinWaiters = 'join-waiters',
+  GetRoomData = 'get-room-data',
+  GetRtpCapabilities = 'get-rtp-capabilities',
+}
+export enum PubSubActions {
+  Message = 'message',
+  EndMeeting = 'end-meeting',
+}
+
+export enum ServiceActions {
+  Close = 'close',
+}
+
+export enum MediaSignalingActions {
+  Connected = 'connected',
+  Heartbeat = 'heartbeat',
+  ServerShutdown = 'server-shutdown',
+}
